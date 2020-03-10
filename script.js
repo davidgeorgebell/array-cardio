f48f1bf41f3539bc74bb270de85545f0be8dab36
davidgeorgebell <bell.dave93@gmail.com>
2020-03-10 08:39:01 +0800

    object and questions

---
+++ script.js
@@ -0,0 +1,21 @@
+const doggos = [
+  { name: 'Scruffy', breed: 'Labradore', age: 3, trained: true },
+  { name: 'Charlie', breed: 'Poodle', age: 8, trained: true },
+  { name: 'Shandy', breed: 'Bulldog', age: 2, trained: false },
+  { name: 'Mossy', breed: 'Border Collie', age: 3, trained: true },
+  { name: 'Winston', breed: 'Bulldog', age: 12, trained: false },
+  { name: 'Humphrey', breed: 'Jack Russel', age: 5, trained: true },
+  { name: 'Spike', breed: 'Boxer', age: 10, trained: true },
+  { name: 'Hunter', breed: 'German Pointer', age: 3, trained: true },
+  { name: 'Marlo', breed: 'Labradore', age: 4, trained: false },
+  { name: 'Dave', breed: 'Bulldog', age: 7, trained: true }
+];
+console.table(doggos);
+
+// 1. Show array of most popular
+// 2. Show youngest to Oldest
+// 3. Names alphabetically
+// 4. Most popular breed
+// 5. Breeds alphabetically
+// 6. Show trained
+// 7. Names ending with 'y'
